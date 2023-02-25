7171dd5a44bca64cbddc5ffbfc50926d3e6cd8c9
Bugigio <gabrieletommasi04@gmail.com>
2023-02-25 13:01:02 +0100

    Incompleted update
    continue the method to change passw on the username.xml

--- Codice/Pagine web/cambiare.php
+++ Codice/Pagine web/cambiare.php
@@ -6,13 +6,16 @@
         if($passwordNuova == $confermaPassword) {
             // trovo il documento xml
             $doc = new DOMDocument();
-            $doc->load('../XML/utenti.xml');
+            $doc->load('../XML/utenti.xml'); // ricerca utente in utenti.xml
             $utenti = $doc->getElementsByTagName('account'); // array degli account
             foreach($utenti as $u) {
                 $username = $u->getElementsByTagName('user')->item(0)->nodeValue;
                 $password = $u->getElementsByTagName('pasw')->item(0)->nodeValue;
                 if($user == $username) {
                     $password = $passwordNuova;
+                    // ricerca file "username".xml
+                    $usernameXML = new DOMDocument();
+                    $usernameXML->load("../XML/utenti/$user.xml");
                     header("location: login.php?passwChanged=true");
                     die();
                 }
