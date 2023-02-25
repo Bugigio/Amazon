37cb55f091ea8f3bde1d3890fb7673f657b0cffa
Bugigio <gabrieletommasi04@gmail.com>
2023-02-25 17:20:43 +0100

    completed update
    fixed and completed the change password method

--- Codice/Pagine web/cambiare.php
+++ Codice/Pagine web/cambiare.php
@@ -1,30 +1,39 @@
 <?php
     if(isset($_POST['submit'])) {
+        session_start();
+        $_SESSION['passwErr'] = 0;
+        session_unset();
         $user = $_POST['user'];
         $passwordNuova = $_POST['passwordNuova'];
         $confermaPassword = $_POST['confermaPassword'];
+
+        // ricerca file "username".xml
+        $usernameXML = new DOMDocument();
+        try {
+            $usernameXML->load("../XML/utenti/$user.xml");
+        } catch (\Throwable $th) {
+            header("location: cambiaPassword.php?err=1&user=$user");
+            die();
+        }
+        // controllo password
         if($passwordNuova == $confermaPassword) {
-            // trovo il documento xml
             $doc = new DOMDocument();
             $doc->load('../XML/utenti.xml'); // ricerca utente in utenti.xml
             $utenti = $doc->getElementsByTagName('account'); // array degli account
             foreach($utenti as $u) {
                 $username = $u->getElementsByTagName('user')->item(0)->nodeValue;
-                $password = $u->getElementsByTagName('pasw')->item(0)->nodeValue;
-                if($user == $username) {
-                    $password = $passwordNuova;
-                    // ricerca file "username".xml
-                    $usernameXML = new DOMDocument();
-                    $usernameXML->load("../XML/utenti/$user.xml");
+                if($username == $user) { 
+                    $u->getElementsByTagName('pasw')->item(0)->nodeValue = $passwordNuova; // cambia la password in utenti.xml
+                    $usernameXML->getElementsByTagName('password')->item(0)->nodeValue = $passwordNuova; // cambia la password in "username".xml
+                    $doc->save("../XML/utenti.xml");
+                    $usernameXML->save("../XML/utenti/$user.xml");
                     header("location: login.php?passwChanged=true");
                     die();
                 }
             }
-            header("location: cambiaPassword.php?err=1&user=$user");
-            die();
         } else {
             header("location: cambiaPassword.php?err=2&user=$user");
-            die();
         }
+
     }
 ?>
