305a23e66703b706b4bfdbcf3b211ca6c5d71842
Bugigio <gabrieletommasi04@gmail.com>
2023-03-02 10:37:04 +0100

    added account's page

--- Codice/Pagine web/shop.php
+++ Codice/Pagine web/shop.php
@@ -12,11 +12,16 @@
         <title>Login con i file</title>
         <link rel="stylesheet" href="../Stili/shop.css">
         <script src="shop.js"></script>
+        <?php
+            session_start();
+            $doc = new DOMDocument();
+            $doc->load('../XML/magazzino.xml');
+        ?>
     </head>
     <body style="margin: 0px;">
         <header>
             <div class="header">
-                <div class="sezione"><a href="#">LIBRI</a></div><div class="sezione"><a href="#">TECNOLOGIA</a></div><div class="sezione"><a href="#">FILM</a></div><div class="sezione"><a href="#">VESTITI</a></div><div class="sezione"><a href="#">SPORT</a></div><div id="pulsante_account"><a href="#">ACCOUNT</a></div>
+                <div class="sezione"><a href="#">LIBRI</a></div><div class="sezione"><a href="#">TECNOLOGIA</a></div><div class="sezione"><a href="#">FILM</a></div><div class="sezione"><a href="#">VESTITI</a></div><div class="sezione"><a href="#">SPORT</a></div><div id="pulsante_account"><a href="account.php">ACCOUNT</a></div>
             </div>
         </header>
         <div class="container">
