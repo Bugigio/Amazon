f03d508638889ce59c74507dd2f289ecdf0f36d8
TOMMASI Gabriele <gabriele.tommasi@serverlab.local>
2023-03-06 10:30:46 +0100

    found solution to divs
    found the solution to divs in the shop page
    
    Co-Authored-By: LolloBara <122354096+LolloBara@users.noreply.github.com>

--- Codice/Pagine web/shop.php
+++ Codice/Pagine web/shop.php
@@ -24,85 +24,58 @@
                 $doc = new DOMDocument();
                 $doc->load('../XML/magazzino.xml');
                 $tagCategoria = $doc->getElementsByTagName($categoria);
-                foreach($tagCategoria as $t) {
-                    $prodotti = $t->getElementsByTagName("prodotto");
-                    foreach($prodotti as $p) {
-                        $nome = $p->getElementsByTagName("nome")->item(0)->nodeValue;
-                        $prezzo = $p->getElementsByTagName("prezzo")->item(0)->nodeValue;
-                    }
-                    break;
-                }
-                
-            } else {
-                header('location: login.php?err=1');
-            }
-        ?>
+                ?>
     </head>
     <body style="margin: 0px;">
         <header>
             <div class="header">
-                <div class="sezione"><a href="#">LIBRI</a></div><div class="sezione"><a href="tecnologia.php?user=<?php echo $_GET['user']; ?>&categoria=tecnologia">TECNOLOGIA</a></div><div class="sezione"><a href="film.php?user=<?php echo $_GET['user']; ?>&categoria=film"">FILM</a></div><div class="sezione"><a href="vestiti.php?user=<?php echo $_GET['user']; ?>&categoria=vestiti">VESTITI</a></div><div class="sezione"><a href="sport.php?user=<?php echo $_GET['user']; ?>&categoria=sport">SPORT</a></div><div id="pulsante_account"><a href="account.php?user=<?php echo $_GET['user'];?>">ACCOUNT</a></div>
+                <div class="sezione"><a href="#">LIBRI</a></div>
+                <div class="sezione"><a href="tecnologia.php?user=<?php echo $_GET['user']; ?>&categoria=tecnologia">TECNOLOGIA</a></div>
+                <div class="sezione"><a href="film.php?user=<?php echo $_GET['user']; ?>&categoria=film"">FILM</a></div>
+                <div class="sezione"><a href="vestiti.php?user=<?php echo $_GET['user']; ?>&categoria=vestiti">VESTITI</a></div>
+                <div class="sezione"><a href="sport.php?user=<?php echo $_GET['user']; ?>&categoria=sport">SPORT</a></div>
+                <div id="pulsante_account"><a href="account.php?user=<?php echo $_GET['user'];?>">ACCOUNT</a></div>
             </div>
         </header>
-        <div class="container">
-            <div class="articolo">
-                <div class="immagine_articolo"><img src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt=""></div>
-                <div class="descrizione_articolo"><h3><?php ?></h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur, ex id ultricies ultricies, metus mauris ultrices nisl, vitae volutpat felis odio in libero. Sed dapibus nec elit quis pulvinar. In faucibus nisl ac fermentum egestas. Quisque hendrerit nibh in velit faucibus tempor. Donec consequat diam vitae nisl sagittis, in mollis ante varius. Suspendisse vestibulum pretium massa, ut tristique lorem laoreet non. Nullam at mi ut turpis fermentum feugiat eget sodales tortor. Integer rhoncus dolor ut ante hendrerit feugiat.</div>
-                <div class="quantita_acquisto_articolo">
-                    <div class="box_quantita">
-                        <button class="bottone" onclick="aumenta(0)">+</button>
-                        <input type="text" name="quantita" class="numero_quantita" min="0" max="10" value="0" readonly>
-                        <button class="bottone" onclick="diminuisci(0)">-</button>
+        <form action="ordina.php?user=<?php echo $_REQUEST['user'];?>" method="post">
+            <div class="container">
+                    <?php
+                        $i = 1;
+                        foreach($tagCategoria as $t) {
+                            $prodotti = $t->getElementsByTagName("prodotto");
+                            foreach($prodotti as $p) {
+                                $nome = $p->getElementsByTagName("nome")->item(0)->nodeValue;
+                                $prezzo = $p->getElementsByTagName("prezzo")->item(0)->nodeValue;
+                    ?>
+                <div class="articolo">
+                    <div class="immagine_articolo"><img src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt=""></div>
+                    <div class="descrizione_articolo"><h3 class="titolo"><?php echo $nome;?></h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                                                                    Curabitur efficitur, ex id ultricies ultricies, metus mauris ultrices nisl, vitae volutpat felis odio in libero.
+                                                                                                    Sed dapibus nec elit quis pulvinar. In faucibus nisl ac fermentum egestas. Quisque hendrerit nibh in velit faucibus tempor.
+                                                                                                    Donec consequat diam vitae nisl sagittis, in mollis ante varius.
+                                                                                                    Suspendisse vestibulum pretium massa, ut tristique lorem laoreet non. Nullam at mi ut turpis fermentum feugiat eget sodales tortor.
+                                                                                                    Integer rhoncus dolor ut ante hendrerit feugiat.<br> <input class="prezzo_articolo" type="number" name="prezzo_articolo<?php echo $i ?>" value="<?php echo $prezzo; ?>€" readonly> </div>
+                    <div class="quantita_acquisto_articolo">
+                        <div class="box_quantita">
+                            <button class="bottone" onclick="aumenta(0)">+</button>
+                            <input type="text" name="quantita" class="numero_quantita" min="0" max="10" value="0" readonly>
+                            <button class="bottone" onclick="diminuisci(0)">-</button>
+                        </div>
                     </div>
                 </div>
+                <?php
+                                $i++;
+                            }
+                            break;
+                        }
+                    } else {
+                        header('location: login.php?err=1');
+                    }
+                ?>
             </div>
-            <div class="articolo">
-                <div class="immagine_articolo"><img src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt=""></div>
-                <div class="descrizione_articolo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur, ex id ultricies ultricies, metus mauris ultrices nisl, vitae volutpat felis odio in libero. Sed dapibus nec elit quis pulvinar. In faucibus nisl ac fermentum egestas. Quisque hendrerit nibh in velit faucibus tempor. Donec consequat diam vitae nisl sagittis, in mollis ante varius. Suspendisse vestibulum pretium massa, ut tristique lorem laoreet non. Nullam at mi ut turpis fermentum feugiat eget sodales tortor. Integer rhoncus dolor ut ante hendrerit feugiat.</div>
-                <div class="quantita_acquisto_articolo">
-                    <div class="box_quantita">
-                        <button class="bottone" onclick="aumenta(1)">+</button>
-                        <input type="text" name="quantita" class="numero_quantita" min="0" max="10" value="0" readonly>
-                        <button class="bottone" onclick="diminuisci(1)">-</button>
-                    </div>
-                </div>
-            </div>
-            <div class="articolo">
-                <div class="immagine_articolo"><img src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt=""></div>
-                <div class="descrizione_articolo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur, ex id ultricies ultricies, metus mauris ultrices nisl, vitae volutpat felis odio in libero. Sed dapibus nec elit quis pulvinar. In faucibus nisl ac fermentum egestas. Quisque hendrerit nibh in velit faucibus tempor. Donec consequat diam vitae nisl sagittis, in mollis ante varius. Suspendisse vestibulum pretium massa, ut tristique lorem laoreet non. Nullam at mi ut turpis fermentum feugiat eget sodales tortor. Integer rhoncus dolor ut ante hendrerit feugiat.</div>
-                <div class="quantita_acquisto_articolo">
-                    <div class="box_quantita">
-                        <button class="bottone" onclick="aumenta(2)">+</button>
-                        <input type="text" name="quantita" class="numero_quantita" min="0" max="10" value="0" readonly>
-                        <button class="bottone" onclick="diminuisci(2)">-</button>
-                    </div>
-                </div>
-            </div>
-            <div class="articolo">
-                <div class="immagine_articolo"><img src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt=""></div>
-                <div class="descrizione_articolo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur, ex id ultricies ultricies, metus mauris ultrices nisl, vitae volutpat felis odio in libero. Sed dapibus nec elit quis pulvinar. In faucibus nisl ac fermentum egestas. Quisque hendrerit nibh in velit faucibus tempor. Donec consequat diam vitae nisl sagittis, in mollis ante varius. Suspendisse vestibulum pretium massa, ut tristique lorem laoreet non. Nullam at mi ut turpis fermentum feugiat eget sodales tortor. Integer rhoncus dolor ut ante hendrerit feugiat.</div>
-                <div class="quantita_acquisto_articolo">
-                    <div class="box_quantita">
-                        <button class="bottone" onclick="aumenta(3)">+</button>
-                        <input type="text" name="quantita" class="numero_quantita" min="0" max="10" value="0" readonly>
-                        <button class="bottone" onclick="diminuisci(3)">-</button>
-                    </div>
-                </div>
-            </div>
-            <div class="articolo">
-                <div class="immagine_articolo"><img src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt=""></div>
-                <div class="descrizione_articolo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur, ex id ultricies ultricies, metus mauris ultrices nisl, vitae volutpat felis odio in libero. Sed dapibus nec elit quis pulvinar. In faucibus nisl ac fermentum egestas. Quisque hendrerit nibh in velit faucibus tempor. Donec consequat diam vitae nisl sagittis, in mollis ante varius. Suspendisse vestibulum pretium massa, ut tristique lorem laoreet non. Nullam at mi ut turpis fermentum feugiat eget sodales tortor. Integer rhoncus dolor ut ante hendrerit feugiat.</div>
-                <div class="quantita_acquisto_articolo">
-                    <div class="box_quantita">
-                        <button class="bottone" onclick="aumenta(4)">+</button>
-                        <input type="text" name="quantita" class="numero_quantita" min="0" max="10" value="0" readonly>
-                        <button class="bottone" onclick="diminuisci(4)">-</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <footer>
-            <div class="footer"><a href="login.php"><button class="ordina"><h2>ORDINA</h2></button></a></div>
-        </footer>
+            <footer>
+                <div class="footer"><input name="submit" type="submit" value="ORDINA"></div>
+            </footer>
+        </form>
     </body>
 </html>
