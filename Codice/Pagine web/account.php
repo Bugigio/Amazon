c6675424a56178468a48b321a5122251fb63de79
LolloBara <lorenzobaratt@gmail.com>
2023-03-08 19:01:21 +0100

    added recharge credit method
    Co-Authored-By: Bugigio <120679842+Bugigio@users.noreply.github.com>

--- Codice/Pagine web/account.php
+++ Codice/Pagine web/account.php
@@ -7,9 +7,15 @@
         <title>Account</title>
         <link rel="stylesheet" href="../Stili/account.css">
         <?php
+            if(!isset($_REQUEST['user'])) {
+                header("location: login.php?err=1");
+                die();
+            }
             $account = new DOMDocument();
             $account->load("../XML/utenti/" . $_REQUEST['user'] .  ".xml");
-
+            if(isset($_REQUEST['ricarica'])) {
+                $account->getElementsByTagName("saldo")->item(0)->nodeValue += $_REQUEST['credito_da_ricaricare'];
+            }
         ?>
     </head>
     <body style="margin: 0px;">
@@ -23,6 +29,10 @@
             <div id="descrizione_utente">
                 <h3>Nome utente</h3>
                 <h4>Saldo: <?php echo $account->getElementsByTagName('saldo')->item(0)->nodeValue;?></h4>
+                <form action="<?php echo $_SERVER['PHP_SELF'];?>?user=<?php echo $_REQUEST['user'];?>" method="post">
+                    <input type="number" name="credito_da_ricaricare" min="0" max="1000">
+                    <input type="submit" name="ricarica" value="Ricarica">
+                </form>
             </div>
         </div>
         <div class="ordini_container">
@@ -30,7 +40,8 @@
         </div>
         
         <?php
-
+            $account->schemaValidate("../XSD/username.xsd");
+            $account->save("../XML/utenti/". $_REQUEST['user'] .".xml");
         ?>
     </body>
 </html>
