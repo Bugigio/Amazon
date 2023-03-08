b0922a25c40d6f4912691ca033ac55ad7e7062a4
LolloBara <lorenzobaratt@gmail.com>
2023-03-08 17:36:24 +0100

    updated ordina.php methods
    Co-Authored-By: Bugigio <120679842+Bugigio@users.noreply.github.com>

--- Codice/Pagine web/ordina.php
+++ Codice/Pagine web/ordina.php
@@ -12,14 +12,28 @@
         }
         $tagCategoria = $magazzino->getElementsByTagName($_REQUEST['categoria']);
         $indice_prodotto = 1;
-        foreach($tagCategoria as $t) {
-            $prodotto = $t->getElementsByTagName("prodotto")->getElementsByTagName("quantita")->nodeValue;
-            if($_REQUEST["quantita_articolo$indice_prodotto"] > 0) {
-                if($_REQUEST["quantita_articolo$indice_prodotto"] > $prodotto) {
-                    header("location: " . $_REQUEST['categoria'] . ".php?user=" . $_REQUEST['user'] . "&categoria=". $_REQUEST['categoria'] . "&err=2");
+        foreach($tagCategoria as $t) { // scorriamo le categorie
+            $prodotto = $t->getElementsByTagName("prodotto"); // prendo i tag prodotto
+            foreach($prodotto as $p) { // scorro i prodotti della categoria
+                $quantita = $p->getElementsByTagName("quantita")->item(0)->nodeValue; // quantita del prodotto $indice_prodotto
+                if($_REQUEST["quantita_articolo$indice_prodotto"] > 0) {
+                    if($_REQUEST["quantita_articolo$indice_prodotto"] > $quantita) {
+                        header("location: " . $_REQUEST['categoria'] . ".php?user=" . $_REQUEST['user'] . "&categoria=". $_REQUEST['categoria'] . "&err=2");
+                        die();
+                    }
+                    $p->getElementsByTagName("quantita")->item(0)->nodeValue -= $_REQUEST["quantita_articolo$indice_prodotto"];
                 }
             }
+            break;
         }
+        $usernameXML->getElementsByTagName("saldo")->item(0)->nodeValue -= $_REQUEST['prezzo_totale']; // sottrazione saldo all'account
+        // validazione e salvataggio
+        $usernameXML->schemaValidate("../XSD/username.xsd");
+        $magazzino->schemaValidate("../XSD/magazzino.xsd");
+        $usernameXML->save("../XML/" . $_REQUEST['user'] . ".xml");
+        $magazzino->save("../XML/magazzino.xml");
+        header("location: " . $_REQUEST['categoria'] . ".php?user=" . $_REQUEST['user'] . "&categoria=" . $_REQUEST['categoria']);
+        die();
     } else {
         header("location: login.php?err=1");
     }
